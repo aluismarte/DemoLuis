8756be5829ad1a6b75f2097651c981c71804c3cd
Aluis Marte <aluismarte@gmail.com>
2019-11-20 21:55:00 -0400

    Creando la base de la app.

--- app/src/main/java/edu/alsjava/courses/demoluis/MainActivity.kt
+++ app/src/main/java/edu/alsjava/courses/demoluis/MainActivity.kt
@@ -1,13 +1,57 @@
 package edu.alsjava.courses.demoluis
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import edu.alsjava.courses.demoluis.ui.IntroActivity
+import edu.alsjava.courses.demoluis.ui.LoginActivity
+import edu.alsjava.courses.demoluis.ui.PrivacyActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postAtTime(Runnable {
+            // Abro la actividad correcta
+            // Compruebo que la app se abra por primera vez.
+            // Compruebo que me aceptaron el privacy
+            // Compruebo si voy al login o la app, es decir, si guardo session
+        }, 1200)
+    }
+
+    fun openIntro() {
+        val intent = Intent(this, IntroActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
+    }
+
+    fun openPrivacy() {
+        val intent = Intent(this, PrivacyActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
+    }
+
+    fun openLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
+    }
+
+    fun myFunction() {
+        val texto = "dgodkogfkogkdofkgd"
+        var cambioDato: String? = null
+    }
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun voidFuntion() {
+        println("Hola soy void")
     }
 }
---
+++ app/src/main/java/edu/alsjava/courses/demoluis/utils/SharedPreferencesTool.java
@@ -0,0 +1,11 @@
+package edu.alsjava.courses.demoluis.utils;
+
+/**
+ * Elemento encargado de manejar las prefencias internas de la aplicacion
+ *
+ * Created by aluis on 11/20/19.
+ */
+public class SharedPreferencesTool {
+
+    // Debes guardar data en el Shared Preferences para saber el estado de la app
+}
